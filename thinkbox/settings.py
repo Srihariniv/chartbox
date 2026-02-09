d6e1075ff550e0e248db8ddb115467d46d392bc8
Sri Harini <sriharini302003@gmail.com>
2026-02-09 14:19:29 +0530

    Use SQLite instead of MySQL for Render

--- thinkbox/settings.py
+++ thinkbox/settings.py
@@ -74,19 +74,17 @@ WSGI_APPLICATION = 'thinkbox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'thinkbox_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
